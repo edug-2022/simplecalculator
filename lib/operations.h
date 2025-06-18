280799ebc04f16f31a823853dfa25e7a8622a142
Edu Garay <@edug-2022>
2025-06-18 13:49:58 -0600

    Added doxygen documentation the operations.h header

--- lib/operations.h
+++ lib/operations.h
@@ -1,9 +1,33 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
+/**
+ * @brief Sum two integer numbers.
+ * @param number1 Fist number.
+ * @param number2 Second number.
+ * @return Sum result.
+ */
 void sum(int number1, int number2);
+/**
+ * @brief Subtract two integer numbers.
+ * @param number1 Fist number.
+ * @param number2 Second number.
+ * @return Subtraction result.
+ */
 void subtract(int number1, int number2);
+/**
+ * @brief Multiply two integer numbers.
+ * @param number1 Fist number.
+ * @param number2 Second number.
+ * @return Multiplication result.
+ */
 void multiply(int number1, int number2);
+/**
+ * @brief Divide two integer numbers.
+ * @param number1 Fist number.
+ * @param number2 Second number.
+ * @return Quotient integer result with a remainder.
+ */
 void divide(int number1, int number2);
 
 #endif
